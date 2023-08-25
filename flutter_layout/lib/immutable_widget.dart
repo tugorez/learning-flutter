006802061d6115dba36c2bd5b902a1f7769afc68
Juanjo Tugores <tugorez@gmail.com>
2023-08-25 11:30:49 -0700

    Creating immutable objects

---
+++ flutter_layout/lib/immutable_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ImmutableWidget extends StatelessWidget {
+  const ImmutableWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Container(
+          color: Colors.purple,
+          child: Padding(
+            padding: const EdgeInsets.all(50),
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
