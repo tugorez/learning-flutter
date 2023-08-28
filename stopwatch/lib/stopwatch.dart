93a758c3cb35fd6000124095fb108f490e30f769
Juanjo Tugores <tugorez@gmail.com>
2023-08-28 15:40:46 -0700

    Interacting with buttons

--- stopwatch/lib/stopwatch.dart
+++ stopwatch/lib/stopwatch.dart
@@ -11,12 +11,7 @@ class Stopwatch extends StatefulWidget {
 class _StopwatchState extends State<Stopwatch> {
   Timer? timer;
   int seconds = 0;
-
-  @override
-  void initState() {
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
-    super.initState();
-  }
+  bool isTicking = false;
 
   @override
   void dispose() {
@@ -30,19 +25,66 @@ class _StopwatchState extends State<Stopwatch> {
       appBar: AppBar(
         title: const Text('Stopwatch'),
       ),
-      body: Center(
-        child: Text(
-          _timerText,
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _timerText,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: const Text('Start'),
+                onPressed: isTicking ? null : _startTimer,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+              ),
+              const SizedBox(width: 20),
+              TextButton(
+                child: const Text('End'),
+                onPressed: isTicking ? _stopTimer : null,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   String get _timerText => '$seconds ${seconds == 1 ? "second" : "seconds"}';
 
+  void _startTimer() {
+    timer?.cancel();
+    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+
+    setState(() {
+      isTicking = true;
+      seconds = 0;
+    });
+  }
+
+  void _stopTimer() {
+    timer?.cancel();
+    timer = null;
+
+    setState(() {
+      isTicking = false;
+    });
+  }
+
   void _onTick(_) {
-    if (!mounted) return;
     setState(() {
       ++seconds;
     });
